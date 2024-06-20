4a5117b9ddcf7d1bd85bbccb5305591d0b0bfe1b
Acherium <53488838+acherium@users.noreply.github.com>
2024-06-20 10:47:22 +0900

    Update dataproc_cafe.py

--- dataproc_cafe.py
+++ dataproc_cafe.py
@@ -24,8 +24,8 @@ with tab1:
         st.subheader(f"월간 매출 데이터")
         st.bar_chart(df_bymonth.set_index("month"))
     st.subheader("주문 내역")
-    tgl_sort = st.toggle("최근순 정렬", value=True)
-    if tgl_sort:
+    t1_tgl_sort = st.toggle("최근순 정렬", value=True, key="t1_tgl_sort")
+    if t1_tgl_sort:
         st.dataframe(df.sort_values("order_date", ascending=False), use_container_width=True)
     else:
         st.dataframe(df.sort_values("order_date"), use_container_width=True)
@@ -33,7 +33,7 @@ with tab2:
     df_filtered = df.groupby("year")["price"].sum().reset_index()
     st.header(f"연간 매출 데이터")
     st.bar_chart(df_filtered.set_index("year"))
-    st.dataframe(df_filtered, use_container_width=True)
+    st.dataframe(df_filtered.set_index("year"), use_container_width=True)
     tip = st.expander("도표 항목 설명")
     tip.write("- year: 연도\n" +
               "- price: 당해 매출총액\n")
@@ -43,14 +43,19 @@ with tab3:
         df_filtered = df.groupby("month")["price"].sum().reset_index()
         st.header(f"월간 매출 데이터")
         st.bar_chart(df_filtered.set_index("month"))
-        st.dataframe(df_filtered, use_container_width=True)
+        st.dataframe(df_filtered.set_index("month"), use_container_width=True)
     elif t3_sel_type == "연도별":
         t4_sel_year = st.selectbox("연도 선택", map((lambda x: int(x)), filter((lambda x: not math.isnan(x)), sorted(df["year"].unique()))), key="0")
         df_filtered = df[df["year"] == t4_sel_year]
         sal_yearly = df_filtered.groupby("month")["price"].sum().reset_index()
         st.header(f"{t4_sel_year}년 월별 매출 데이터")
         st.bar_chart(sal_yearly.set_index("month"))
-        st.dataframe(df_filtered, use_container_width=True)
+        st.subheader(f"{t4_sel_year}년 주문 내역")
+        t3b_tgl_sort = st.toggle("최근순 정렬", value=True, key="t3b_tgl_sort")
+        if t3b_tgl_sort:
+            st.dataframe(df_filtered.sort_values("order_date", ascending=False), use_container_width=True)
+        else:
+            st.dataframe(df_filtered.sort_values("order_date"), use_container_width=True)
     tip = st.expander("도표 항목 설명")
     tip.write("- order_id: 주문 번호\n" +
               "- order_date: 주문일자\n" +
@@ -66,7 +71,12 @@ with tab4:
     sal_monthly = df_filtered.groupby("month")["price"].sum().reset_index()
     st.header(f"{t5_sel_year}년 월별 [{sel_prod}] 매출 데이터")
     st.bar_chart(sal_monthly.set_index("month"))
-    st.dataframe(df_filtered, use_container_width=True)
+    st.subheader(f"[{sel_prod}]의 {t5_sel_year}년 주문 내역")
+    t2_tgl_sort = st.toggle("최근순 정렬", value=True, key="t2_tgl_sort")
+    if t2_tgl_sort:
+        st.dataframe(df_filtered.sort_values("order_date", ascending=False), use_container_width=True)
+    else:
+        st.dataframe(df_filtered.sort_values("order_date"), use_container_width=True)
     tip = st.expander("도표 항목 설명")
     tip.write("- order_id: 주문 번호\n" +
               "- order_date: 주문일자\n" +
